7235251d00f3f75969e2f31238752c3796a64336
Maksim Memus <maksim.memus@mail.ru>
2022-06-09 11:05:19 +0300

    Added offers tab

---
+++ Asclepius-Client/components/OfferCard.tsx
@@ -0,0 +1,26 @@
+import classNames from "classnames";
+import { ReactSVG } from "react-svg";
+import styles from "../styles/components/OfferCard.module.scss";
+
+interface OfferCardProps {
+    children?: React.ReactNode;
+    className?: string;
+    title?: string;
+    actions?: React.ReactNode;
+}
+
+export default function Offercard({
+    children,
+    className,
+    ...props
+}: OfferCardProps) {
+    return (
+        <div className={classNames([styles.cardBody, className])}>
+            <div className={styles.cardHeader}>
+                <span className={styles.cardTitle}>{props.title}</span>
+                <ReactSVG src="/images/icons/cards/more.svg" className={styles.iconContainer}/>
+            </div>
+            {children}
+        </div>
+    );
+}
