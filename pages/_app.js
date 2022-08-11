5937867995cdf62f1b3edd8892ae0a37c6a2d3ad
NikitoriusMinsk <45353693+NikitoriusMinsk@users.noreply.github.com>
2022-08-11 16:46:36 +0400

    fix import

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import 'styles/globals.css'
-import "styles/components/inputs/select.css";
+import "/styles/components/Inputs/select.css";
 
 function MyApp({ Component, pageProps }) {
-  const getLayout = Component.getLayout || ((page) => page);
+    const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+    return getLayout(<Component {...pageProps} />);
 }
 
 export default MyApp
