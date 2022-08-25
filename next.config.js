52aaceb8b487ba9c8a5e890b27fb5f188377258f
Irina Endeladze <irinaendeladze@MacBook-Pro.local>
2022-08-25 19:06:30 +0400

    made saerch by name

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
 
 module.exports = {
   trailingSlash: true,
@@ -12,17 +11,19 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/asclepius/v1/api/clinics/',
-        destination: 'https://asclepius.pirveli.ge/asclepius/v1/api/clinics/'
+        source: "/asclepius/v1/api/clinics/",
+        destination: "https://asclepius.pirveli.ge/asclepius/v1/api/clinics/",
       },
       {
-        source: '/asclepius/v1/api/clinics/search',
-        destination: 'https://asclepius.pirveli.ge/asclepius/v1/api/clinics/search'
+        source: "/asclepius/v1/api/clinics/search",
+        destination:
+          "https://asclepius.pirveli.ge/asclepius/v1/api/clinics/search",
       },
       {
-        source: '/asclepius/v1/api/clinics/:path*',
-        destination: 'https://asclepius.pirveli.ge/asclepius/v1/api/clinics/:path*'
-      }
-    ]
+        source: "/asclepius/v1/api/clinics/:path*",
+        destination:
+          "https://asclepius.pirveli.ge/asclepius/v1/api/clinics/:path*",
+      },
+    ];
   },
-}
+};
