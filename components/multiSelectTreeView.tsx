16f3d1bafbc70471cdcbee6744074151d4739a7d
Cube00 <coller.vato@gmail.com>
2022-09-21 18:48:43 +0400

    fix requests

--- components/multiSelectTreeView.tsx
+++ components/multiSelectTreeView.tsx
@@ -120,6 +120,9 @@ export default function RichObjectTreeView({
                                             value="add" 
                                             onClick={(e)=>{
                                                 e.preventDefault();
+                                                addPriceToProduct(contractId, nodes?.id, 200)
+                                                    .then((response)=> console.log(response))
+                                                    .catch((error)=> console.log)
                                             }} 
                                         />
                                     </form>
@@ -365,4 +368,26 @@ export function AddServicesModal({
             />
         </Modal>
     </>
+}
+
+export async function addPriceToProduct(contractId, serviceId, price) {
+    try{
+        const req = await fetch(`https://asclepius.pirveli.ge/asclepius/v1/api/transactions/generate-products`, {method: "POST", body: JSON.stringify({
+            "serviceIds": [
+                serviceId
+              ],
+              "contractId": contractId,
+              "price": price,
+              "productDescription": {
+                "titles": {
+                  1: null
+                }
+              }
+        })});
+        const res = req.json();
+
+        return res
+    }catch(error){
+        console.log(error)
+    }
 }
